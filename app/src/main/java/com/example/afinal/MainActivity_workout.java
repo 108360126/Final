632d89d26e8cd7fd1b272d7de4f7b58519d58993
Laixuzhen123 <a33909121@gmail.com>
2022-01-11 15:41:51 +0800

    two

---
+++ app/src/main/java/com/example/afinal/MainActivity_workout.java
@@ -0,0 +1,44 @@
+package com.example.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity_workout extends AppCompatActivity {
+
+    private Button button,button2,button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_workout);
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity_workout.this, MainActivity_easy.class),
+                        1);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity_workout.this, MainActivity_medium.class),
+                        1);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity_workout.this, MainActivity_hard.class),
+                        1);
+            }
+        });
+    }
+}
